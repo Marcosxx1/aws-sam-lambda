d56f1108153a82ccbde1c2d0f36dc90b7a1429e2
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-10-16 13:56:55 -0400

    feat: Init ErrorResponseExample project

---
+++ error-response-example/ErrorResponseExample/src/test/java/com/appsdeveloperblog/aws/errorresponse/lambda/DivisionExampleFunctionTest.java
@@ -0,0 +1,23 @@
+package com.appsdeveloperblog.aws.errorresponse.lambda;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DivisionExampleFunctionTest {
+  @Test
+  public void successfulResponse() {
+    DivisionExampleFunction divisionExampleFunction = new DivisionExampleFunction();
+    APIGatewayProxyResponseEvent result = divisionExampleFunction.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+    assertNotNull(content);
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
