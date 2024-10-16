9b5a7ed28ef3f2efeaf8fdc698e3f59de27d3a38
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-10-16 13:26:57 -0400

    fix: Change Outputs attributes to reflect the actual service. Create Makefile and implement PostHandler

--- DataTransformationExample/DataTransformationExample/src/main/java/com/appsdeveloperblog/aws/datatransformation/lambda/PostHandler.java
+++ DataTransformationExample/DataTransformationExample/src/main/java/com/appsdeveloperblog/aws/datatransformation/lambda/PostHandler.java
@@ -1,17 +1,34 @@
 package com.appsdeveloperblog.aws.datatransformation.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 
 public class PostHandler implements RequestHandler<Map<String, String>, Map<String, String>> {
 
     public Map<String, String> handleRequest(final Map<String, String> input, final Context context) {
 
+        String firstName = input.get("firstName");
+        String lastName = input.get("lastName");
+        String email = input.get("email");
+        String password = input.get("password");
+        String repeatPassword = input.get("repeatPassword");
+
+        LambdaLogger logger = context.getLogger();
+        logger.log("\n firstName: " + firstName);
+        logger.log("\n lastName: " + lastName);
+        logger.log("\n email: " + email);
+
         Map<String, String> response = new HashMap<>();
+        response.put("id", UUID.randomUUID().toString());
+        response.put("firstName", firstName);
+        response.put("lastName", lastName);
+        response.put("email", email);
 
         return response;
 
