61fe44df4cf93710a3d3820adfc28f0fda305663
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-10-16 07:10:33 -0400

    feat: Configure SAM template, PostHandler and pom

---
+++ DataTransformationExample/DataTransformationExample/src/main/java/com/appsdeveloperblog/aws/datatransformation/lambda/PostHandler.java
@@ -0,0 +1,20 @@
+package com.appsdeveloperblog.aws.datatransformation.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PostHandler implements RequestHandler<Map<String, String>, Map<String, String>> {
+
+    public Map<String, String> handleRequest(final Map<String, String> input, final Context context) {
+
+        Map<String, String> response = new HashMap<>();
+
+        return response;
+
+    }
+}
