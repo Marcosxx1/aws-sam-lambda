9a9f0dca92c645e15a0ad25cb1f72397d337cebd
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-10-17 18:17:35 -0400

    feat: implement logic for lambda testing

--- error-response-example-non-proxy/ErrorResponseExampleNonProxy/src/main/java/com/appsdeveloperblog/aws/errorresponse/lambda/DivisionExampleFunction.java
+++ error-response-example-non-proxy/ErrorResponseExampleNonProxy/src/main/java/com/appsdeveloperblog/aws/errorresponse/lambda/DivisionExampleFunction.java
@@ -6,10 +6,19 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DivisionExampleFunction implements RequestHandler<Map<String, String>, Map<String, String>> {
+public class DivisionExampleFunction implements RequestHandler<Map<String, Integer>, Map<String, Integer>> {
 
-    public Map<String, String> handleRequest(final Map<String, String> input, final Context context) {
-        Map<String, String> response = new HashMap<>();
+    public Map<String, Integer> handleRequest(final Map<String, Integer> input, final Context context) {
+
+        Map<String, Integer> response = new HashMap<>();
+
+        int dividend = input.get("dividend");
+        int divisor = input.get("divisor");
+        int result = (int) dividend /divisor;
+
+        response.put("dividend", dividend);
+        response.put("divisor", divisor);
+        response.put("result", result);
 
         return response;
 
