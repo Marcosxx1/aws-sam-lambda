040b3ab616225913fb7c7de4aecaa18200387b5f
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-10-18 11:51:03 -0400

    feat: init project lambda-version-example

---
+++ lambda-version-example/LambdaVersionExample/src/main/java/com/appsdeveloperblog/aws/lambda/LambdaVersionExample.java
@@ -0,0 +1,23 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class LambdaVersionExample implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+
+        return new APIGatewayProxyResponseEvent()
+                .withBody("Code version '3" + context.getFunctionVersion());
+    }
+}
